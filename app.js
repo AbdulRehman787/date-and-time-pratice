dfc69f915c6a50e03c14417112041a8cb337ce5a
AbdulRehman786 <a.rehman78766@gmail.com>
2022-12-22 17:45:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,177 @@
+// var friend = ['Asim','Zahid','Salman']
+// for(let elements in friend){
+//     console.log(elements)
+// }
+
+// for(let elements of friend){
+//     console.log(elements)
+// }
+
+// function data(){
+//     alert("Hye Please enter your bestFriend Name");
+//     let inp1 = prompt("Enter your first  best  Friend");
+//     let inp2 = prompt("Enter your second  best  Friend");
+//     let inp3 = prompt("Enter your third best Friend");
+
+//     var obj=[inp1,inp2,inp3]
+//     for(let elements of obj){
+//         console.log(elements)
+//     }
+    
+// }
+
+// data()
+
+// var friend = ['Asim ','Zahid ','Salman ','sammer ','furqan']
+// friend.forEach(function(elements,index,array){
+//     console.log(`${elements} index ${index} ${array}`)
+
+// })
+
+
+// 
+
+
+// var arr = [3,6,9,12,15];
+
+// let newArr = arr.map((curElm,index,arr)=>{
+//     return curElm >=9;
+// })
+// console.log(arr)
+// console.log(newArr)
+
+
+// var arr = [3,6,9,12,15];
+// let newArr = arr.map((curElem,index,arr)=>{
+//     return `Index no = ${index} and the value is ${curElem} belong to ${arr}`
+// })
+// console.log(newArr)
+
+
+// var arr=[25,36,49,64,81]
+// let newArr = arr.map((curElem)=>{
+//     return Math.sqrt(curElem);
+// })
+// console.log(newArr)
+
+// var arr = [25,36,49,64,81]
+// let newArr = arr.map((curElem)=>{
+//     return Math.sqrt(curElem);
+// })
+// console.log(newArr);
+
+// var arr = [4,9,16,25,36]
+// let newArr = arr.map((curElem)=>{
+//     return Math.sqrt(curElem);
+// })
+// console.log(newArr);
+
+
+
+// var arr = [2,5,6,7,8]
+//  let newArr =arr.map((curElem)=>{
+//     return curElem*2;
+//  }).filter((curElem)=>{
+//     return curElem >=10;
+//  })
+//  console.log(newArr)
+
+// var arr = [2,5,6,8,2];
+// let newArr = arr.map((curElem)=>{
+//     return curElem *2
+// }).filter((curElem)=>{
+//     return curElem >=10
+// })
+
+// console.log(newArr )
+
+
+// var i =0;
+// do{
+//     console.log(i);
+//     i++
+// } while(i<5);
+ 
+// var i =0;
+// while(i<5){
+//     console.log(i)
+//     i++
+// }
+
+// var arr = ['Jan','March','April','May',"June"];
+// arr.push('Feb');
+// console.log(arr)
+
+// var arr = ['Jan','March','April','May',"June"];
+// var arr2 = arr.splice(0,1,'Feb')
+// console.log(arr2)
+
+// var str = ['Jan','March','June','June'];
+// var res = str.replace("June", "success"); 
+// console.log(res)
+
+// var arr= [1,2,3,4,5,6,7]
+// arr.forEach(function(curElem){
+//     console.log(curElem)
+// });
+
+// let elem = document.getElementById('btn');
+// console.log(elem)
+
+// ;
+// let elem =document.getElementsByTagName("div")
+// console.log(elem);
+// creatElement= document.createElement('p')
+// creatElement.innerText = '  This is a button';
+// elem[0].appendChild(creatElement);
+
+// creatElement1= document.createElement('b')
+// creatElement1.innerText = '  This is a bold';
+
+// elem[0].replaceChild(creatElement1,creatElement);
+
+
+// let elem =document.querySelectorAll('.container')
+// console.log(elem)
+
+
+// function clicked(){
+// }
+
+// container.addEventListener('click',function(){
+//     console.log('click huwa')
+// }) 
+
+
+// function abc(){
+//     console.log("its is clicked")
+// }
+// function abco(){
+//     console.log('out of clicks')
+// }
+
+// function sum(a,b){
+//       c= a+b;
+//     console.log(c)
+// }
+// sum(8,9)
+
+// logKaro=()=>{                         ; 
+//     console.log("its a log karo")
+// }
+// // setTimeout(logKaro,1000);
+// // clr =setInterval(logKaro,2000)
+// setTimeout(logKaro, 3000 )
+
+
+// const person = {
+//     name: "Obaseki Nosa",
+//     location: "Lagos",
+// }
+
+// window.localStorage.setItem('user', JSON.stringify(person));
+
+
+
+// localStorage.clear();
+
